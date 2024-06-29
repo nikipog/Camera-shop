3c20ec26299b7da8bf7d056b8b491a8d90cf0f64
Никита Погорелов <nikipog@mail.ru>
2024-06-29 20:22:31 +0300

    feat: add search accessibility

--- src/components/search-item/search-item.tsx
+++ src/components/search-item/search-item.tsx
@@ -1,10 +1,45 @@
+import { useNavigate, generatePath } from 'react-router-dom';
 import { Product } from '../../types/product';
+import { AppRoute } from '../../const';
 
-const SearchItem = ({ product }: { product: Product }): JSX.Element => (
-  <li className="form-search__select-item" tabIndex={0}>{product.name}</li>
-);
+type SearchItemProps = {
+  product: Product;
+  onSearchResultClick: () => void;
+  focusRef: (el: HTMLLIElement | null) => void;
+}
+
+const SearchItem = ({ product, onSearchResultClick, focusRef }: SearchItemProps): JSX.Element => {
+
+  const url = generatePath(AppRoute.Product, { id: product.id.toString() });
+  const navigate = useNavigate();
+
+  const handleSearchResultClick = () => {
+    navigate(url);
+    onSearchResultClick();
+  };
+
+  const handleSearchItemKeyDown = (event: React.KeyboardEvent<HTMLLIElement>) => {
+    if (event.key === 'Enter') {
+      handleSearchResultClick();
+    }
+  };
 
 
-SearchItem.displayName = 'SearchItem';
+  return (
+
+    <li
+      className="form-search__select-item"
+      tabIndex={0}
+      onClick={handleSearchResultClick}
+      onKeyDown={handleSearchItemKeyDown}
+      ref={focusRef}
+    >
+      {product.name}
+    </li>
+
+
+  );
+};
+
 
 export default SearchItem;
